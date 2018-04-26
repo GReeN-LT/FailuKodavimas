7aef199b311d7278e38bfcc489c5aab0ebe736f3
GReeN-LT <mindaugas.laboga23@gmail.com>
2018-04-26 11:01:01 +0300

    Add project files.

---
+++ FailuKodavimasPraktinis2/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace FailuKodavimasPraktinis2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pasirinkti katalogą";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(63, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Failų užkodavimas/atkodavimas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pasirinkta: nieko";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Rasta failų: 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Koduoti failus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Atkoduoti failus";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kodavimo slaptažodis:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(379, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(379, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Išeiti";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(67, 267);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(442, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(67, 227);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Pauzė";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(224, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Tęsti";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(379, 227);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(130, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Stabdyti";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 344);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ FailuKodavimasPraktinis2/Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+
+
+namespace FailuKodavimasPraktinis2
+{
+    public partial class Form1 : Form
+    {
+
+        private ManualResetEvent mre;
+        private Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.UseSystemPasswordChar = true;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pasirinkt kataloga
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+               
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+               
+                label2.Visible = true;
+                label3.Visible = true;
+                label2.Text = "Pasirinkta: " + folderBrowserDialog1.SelectedPath;
+                label3.Text = "Failų rasta: " + files.Length.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //koduot failus
+            // zip iskviest
+
+            if (thread == null || thread?.IsAlive == false)
+            {
+                thread = new Thread(new ThreadStart(zipKodavimas));
+                thread.Start();
+                thread.IsBackground = true;
+                mre = new ManualResetEvent(true);
+               
+            }
+
+        }
+
+
+    
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //atkoduoti failus
+
+            if (thread == null || thread?.IsAlive == false)
+            {
+                thread = new Thread(new ThreadStart(unZipAtodavimas));
+                thread.Start();
+                thread.IsBackground = true;
+                mre = new ManualResetEvent(true);
+               
+            }
+
+        }
+
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public void zipKodavimas()
+        {
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                progressBar1.Value = 0;
+            });
+
+            string startPath = folderBrowserDialog1.SelectedPath;
+
+            mre.WaitOne();
+
+
+            string zipPath = folderBrowserDialog1.SelectedPath + ".zip";
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            mre.WaitOne();
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            string file = folderBrowserDialog1.SelectedPath + ".zip";
+
+            mre.WaitOne();
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+
+            
+
+            DirectoryInfo di = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            //delete files after zip 
+            foreach (FileInfo files in di.GetFiles())
+            {
+                files.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+            //delete files
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            mre.WaitOne();
+
+            string password = textBox1.Text;
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file); //turi rast kodavimas.zip desktope
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            mre.WaitOne();
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            passwordBytes = MD5.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            string hashPassword = BitConverter.ToString(passwordBytes).Replace("-", ""); //hash reiksme
+            //MessageBox.Show(hashPassword);
+
+            using (StreamWriter writer =
+            new StreamWriter("hash.txt"))
+            {
+
+                writer.WriteLine(hashPassword);
+
+            }
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            mre.WaitOne();
+
+            //MessageBox.Show(MD5.Create().ComputeHash(passwordBytes).ToString());
+
+            //deletint specifini faila
+            File.Delete(folderBrowserDialog1.SelectedPath + ".zip");
+
+            //deletint specifini faila
+
+            string fileEncrypted = folderBrowserDialog1.SelectedPath + "Coded.zip"; //uzkoduotas failas
+
+            File.WriteAllBytes(fileEncrypted, bytesEncrypted);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            MessageBox.Show("Failai užkoduoti!");
+        }
+
+        void unZipAtodavimas()
+        {
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                progressBar1.Value = 0;
+            });
+
+
+            string fileEncrypted = folderBrowserDialog1.SelectedPath + "Coded.zip"; //uzkoduotas failas
+
+            string password = textBox1.Text;
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            textBox1.UseSystemPasswordChar = true;
+
+            mre.WaitOne();
+
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(fileEncrypted);
+
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            passwordBytes = MD5.Create().ComputeHash(passwordBytes);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            mre.WaitOne();
+
+            string file = folderBrowserDialog1.SelectedPath + "UnCoded.zip"; //atkoduot ir išzipint 
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            File.WriteAllBytes(file, bytesDecrypted);
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            mre.WaitOne();
+
+            //unzip
+            // ZipFile.ExtractToDirectory(folderBrowserDialog1.SelectedPath + "UnCoded.zip", "C:\\Users\\Mindaugas Laboga\\Desktop\\atkoduotiFailai"); //alredy exist 
+            ZipFile.ExtractToDirectory(folderBrowserDialog1.SelectedPath + "UnCoded.zip", folderBrowserDialog1.SelectedPath); //alredy exist 
+
+            //unzip
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+
+            File.Delete(folderBrowserDialog1.SelectedPath + "UnCoded.zip");
+
+            mre.WaitOne();
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            File.Delete(folderBrowserDialog1.SelectedPath + "Coded.zip");
+
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                updateProgressBar();
+            });
+
+            MessageBox.Show("Failai atkoduoti!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //isejimas
+
+            Application.Exit();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //pauze
+
+            mre.Reset(); 
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //testi
+
+            mre.Set();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //stabdyti
+            thread.Abort();
+            
+        }
+
+        void updateProgressBar()
+        {
+            progressBar1.Value += 10;
+
+        }
+
+    }
+}
